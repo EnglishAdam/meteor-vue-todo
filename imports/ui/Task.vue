dd948e0ccd5bfb56deca6082a61d2533c00a0e2a
Adam English <englishadam@hotmail.co.uk>
2019-05-09 23:19:06 +0100

    Updated task schema and added test component

---
+++ imports/ui/Task.vue
@@ -0,0 +1,40 @@
+<template>
+  <li :class="taskClassName">
+    <button class="delete" @click="deleteThisTask">&times;</button>
+    <!-- Delete button -->
+
+    <input type="checkbox" readOnly :checked="task.checked" @click="toggleChecked" />
+    <!-- Text Input -->
+
+    <span className="text">{{task.text}}</span>
+    <!-- Test -->
+  </li>
+</template>
+
+<script>
+import { Meteor } from 'meteor/meteor';
+
+export default {
+  props: [
+    'task'
+  ],
+
+  computed: {
+    taskClassName() {
+      if (this.task.checked) return 'checked'
+      return ''
+    }
+  },
+
+  methods: {
+    toggleChecked() {
+      // Set the checked property to the opposite of its current value
+      Meteor.call('tasks.setChecked', this.task._id, !this.task.checked);
+    },
+
+    deleteThisTask() {
+      Meteor.call('tasks.remove', this.task._id);
+    }
+  }
+}
+</script>
