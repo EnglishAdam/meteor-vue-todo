92883b37e1a640bb1ac99980a7e8c4d356d558a4
Adam English <englishadam@hotmail.co.uk>
2019-05-09 22:46:02 +0100

    Added startup code

--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
+import { Time } from '../imports/api/time.js';
+
 Meteor.startup(() => {
-  // code to run on server at startup
+    // Update the current time
+    Meteor.call('time.update');
+    // Add a new doc on each start.
+    Time.insert({ time: new Date() });
+    // Print the current time from the database
+    console.log(`The time is now ${Time.findOne().time}`);
 });
